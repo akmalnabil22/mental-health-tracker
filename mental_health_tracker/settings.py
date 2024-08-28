b2c1c6dbaabd631c2cc68733663f4b9b8827e22f
akmalnabil22 <akmaln761@gmail.com>
2024-08-28 16:25:16 +0700

    add URL Deployment URL to ALLOWED_HOST

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&aillg7_vf5c)%2-!xrbe4rnff*q0c&&mrp#6ea(=i7rc=7#u4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "akmal-nabil-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
